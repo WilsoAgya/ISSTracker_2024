54d1548d7dbb82c9c7069421303fd1a51443e3d7
LAPTOP-VJRUABD9\aiden <aidenrcarelse@gmail.com>
2024-09-28 13:03:17 -0700

    updated navbar

--- iss_tracker/src/components/Nav.jsx
+++ iss_tracker/src/components/Nav.jsx
@@ -1,10 +1,9 @@
 import react from 'react';
-import '../App.css'; 
 
 const Navbar = () => {
 
     return(
-        <nav id="navbar" class="bg-dark navbar">
+        <nav id="navbar" className="navbar">
             <div className="py-1 px-3 container-fluid d-flex align-items-center h-100">
                 <h1 id="nav_title">ISS Tracker</h1>
             </div>
