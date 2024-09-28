db8e131e0fa917b5de8237f7bcdccee732f2074a
LAPTOP-VJRUABD9\aiden <aidenrcarelse@gmail.com>
2024-09-28 14:33:54 -0700

    Minor UI changes

--- iss_tracker/src/components/Main.jsx
+++ iss_tracker/src/components/Main.jsx
@@ -1,10 +1,10 @@
 const Main= ()=>{
     return(
         <div className="row mx-auto my-5 g-3">
-        <div className="col-10 col-md-6 bg-white mx-auto rounded-3 border" style={{ height: '400px' }}>
+        <div className="col-10 col-md-6 bg-dark mx-auto rounded-3 shadow" style={{ height: '400px' }}>
             GOOGLE MAP 
         </div>
-        <div className="col-10 col-md-4 bg-dark mx-auto d rounded-3 py-3 px-5">
+        <div className="col-10 col-md-4 bg-dark mx-auto d rounded-3 py-3 px-5 shadow">
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <div className='lighter p-2 mt-3 mb-1 mx-5 rounded-2'>
                     <h1 className='text-white timer px-5'>00:00:00</h1>
