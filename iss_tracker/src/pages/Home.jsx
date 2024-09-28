eb2db0a0166868abb0900b72a99a8ebcfd4605b8
Baljeet Singh <22825252+sbaljeet13@users.noreply.github.com>
2024-09-28 15:49:26 -0700

    Remove QueryComp

--- iss_tracker/src/pages/Home.jsx
+++ iss_tracker/src/pages/Home.jsx
@@ -1,22 +1,17 @@
 /* eslint-disable no-unused-vars */
-import react from "react";
-import QueryComp from "../components/QueryComp";
 
 import Navbar from '../components/Nav';
 import Main from '../components/Main';
 import Map from '../components/Map';
 import '../App.css'; 
+
 const Home = () => {
-  
-  
-  
   return (
     <>
         <div id='background'>
             <Navbar />
             <Main />
         </div>
-        <QueryComp />
         <Map />
     </>
   );
