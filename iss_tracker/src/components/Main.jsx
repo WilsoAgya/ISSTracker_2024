2a6f89b8f584d0c1b9aab595635587a9fdd36e84
Deol7777 <58061160+Deol7777@users.noreply.github.com>
2024-09-28 14:47:17 -0700

    info component working

--- iss_tracker/src/components/Main.jsx
+++ iss_tracker/src/components/Main.jsx
@@ -1,3 +1,5 @@
+import InfoComponent from "./InfoComponent"
+
 const Main= ()=>{
     return(
         <div className="row mx-auto my-5 g-3">
@@ -5,18 +7,7 @@ const Main= ()=>{
             GOOGLE MAP 
         </div>
         <div className="col-10 col-md-4 bg-dark mx-auto d rounded-3 py-3 px-5">
-            <div className='d-flex flex-column justify-content-center align-items-center'>
-                <div className='lighter p-2 mt-3 mb-1 mx-5 rounded-2'>
-                    <h1 className='text-white timer px-5'>00:00:00</h1>
-                </div>
-                <p className='text-white mb-1'>Time until the ISS passes LOCATION</p>
-                <p className='text-white mb-5'>Last updated at TIME</p>
-            </div>
-            
-            <h4 className='text-white mb-3'>• Longitude: 000000</h4>
-            <h4 className='text-white mb-3'>• Latitude: 000000</h4>
-            <h4 className='text-white mb-3'>• Altitude: 000000</h4>
-            <h4 className='text-white mb-3'>• Speed: 000000</h4>
+            <InfoComponent />
         </div>
     </div>
     )
