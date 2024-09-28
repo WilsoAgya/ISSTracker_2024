3f5a5f41a4e46f39428d1d0ad2008192106a106f
Deol7777 <58061160+Deol7777@users.noreply.github.com>
2024-09-28 13:30:18 -0700

    fix the white space at bottom

--- iss_tracker/src/components/Main.jsx
+++ iss_tracker/src/components/Main.jsx
@@ -1,8 +1,6 @@
-import react from 'react';
-
 const Main= ()=>{
     return(
-        <div className="row mx-auto my-5 g-3">
+        <div className="row mx-auto g-3 py-2">
     <div className="col-10 col-md-7 bg-white mx-auto  order-md-1 order-2 rounded-3 border" style={{ height: '400px' }}>
         GOOGLE MAP 
     </div>
