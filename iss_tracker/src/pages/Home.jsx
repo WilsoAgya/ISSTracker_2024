9adfabfbec5a8fee1ea96098f8b1313440230796
LAPTOP-VJRUABD9\aiden <aidenrcarelse@gmail.com>
2024-09-28 16:32:32 -0700

    Added timer to UI

--- iss_tracker/src/pages/Home.jsx
+++ iss_tracker/src/pages/Home.jsx
@@ -5,21 +5,15 @@ import InfoComponent from "../components/InfoComponent";
 import Navbar from '../components/Nav';
 import Main from '../components/Main';
 import '../App.css'; 
-import NextPass from "../components/NextPass";
 
 const Home = () => {
   return (
     <>
       <div id='background'>
-
         <div className="main"></div>
-
-     
-
             <Navbar />
             <Main />
         </div>
-        <NextPass/>
     </>
   );
 };
