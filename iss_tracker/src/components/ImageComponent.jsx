0438aab3d5211c9ffcb0b7bcd152f43af0378513
Deol7777 <58061160+Deol7777@users.noreply.github.com>
2024-09-28 17:48:13 -0700

    calendar working

--- iss_tracker/src/components/ImageComponent.jsx
+++ iss_tracker/src/components/ImageComponent.jsx
@@ -1,9 +1,34 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import BasicDatePicker from "./Calendar";
+import { useState } from "react";
 const ImageComponent = () => {
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+// Create a Date object from a specific date
+  const dateString = '2024-09-27'; // Input date in YYYY-MM-DD format
+  const dateObject = new Date(dateString); // This creates a Date object
+  
+//experimental
+  const [date, setDate] = useState(formatDate(dateObject));
+
+  const handleDateChange = (newDate) => {
+
+    const temp = new Date(newDate.format('YYYY-MM-DD'));
+    setDate(formatDate(temp));
+    console.log('Date in Parent:', newDate.format('YYYY-MM-DD')); // Log the date in YYYY-MM-DD format
+  };
+  console.log(formatDate(dateObject))
+
   const fetchDataImage = async () => {
   const apiKey = import.meta.env.VITE_NASA_API_KEY;
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+  //const date = formatDate(dateObject);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
   const response = await axios.get(url);
   return response.data;
   };
@@ -24,6 +49,7 @@ const ImageComponent = () => {
         <h2 className="my-2 text-light">{dataImage.title}</h2>
         <img className="my-2 border border-light" src={dataImage.url} style={{ maxHeight: '600px', height: 'auto' }} alt="NASA" />
         <p className="my-2 text-light">{dataImage.explanation}</p>
+        <BasicDatePicker onDateChange={handleDateChange}/>
     </div>
   );
 };
